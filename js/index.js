20cb15bb9aea84f0b3cdfb6dd838c7756fa7bce1
davidzabalperez <davidzabalperez@gmail.com>
2019-07-30 19:40:28 +0100

    fotos elektrika bataplan

--- js/index.js
+++ js/index.js
@@ -9,8 +9,14 @@ var time= 3000; //tiempo en milisegundos
 // image list
 images[0]="../img/fotosJontxu5.jpg";
 images[1]="../img/fotosJontxu2.jpg";
-images[2]="../img/fotosJontxu3.jpg";  
-images[3]="../img/fotosJontxu4.jpg";
+images[2]="../img/fotosJontxu3.jpg";
+images[3]="../img/fotosJontxu6.jpg";
+images[4]="../img/fotosJontxu7.jpg";
+images[5]="../img/fotosJontxu8.jpg";
+images[6]="../img/fotosJontxu9.jpg";
+images[7]="../img/fotosJontxu10.jpg";
+images[8]="../img/fotosJontxu11.jpg";
+images[9]="../img/fotosJontxu12.jpg";
 
 //create function
 
